6b406c570f68160ce9dddc1197b69c4512be97de
Dima_Karpov <amail930@gmail.com>
2024-03-10 14:58:41 +0200

    add .env file from token tg

--- app.py
+++ app.py
@@ -2,28 +2,27 @@ import asyncio
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
+from dotenv import find_dotenv, load_dotenv
 
-bot = Bot(token='6613317533:AAGSJkzelDPIJyJzKAa20TicWOKkZs8sO2Q')
+load_dotenv(find_dotenv())
 
 dp = Dispatcher()
+
 
 @dp.message(CommandStart())
 async def start_cmd(message: types.Message):
     await message.answer('It was the start command')
 
+
 @dp.message()
 async def echo(message: types.Message):
-    text = message.text
-
-    if text in ['Привет', 'привет', 'Hi', 'hi', 'Hello', 'hello']:
-        await message.answer(f'{text} to you too')
-    elif text in ['Пока', 'пока', 'Bye', 'bye']:
-        await message.answer(f'{text} to you too')
-    else:
-        await message.answer(message.text)
+    await message.answer(message.text)
+    await message.reply(message.text)
 
 
 async def main() -> None:
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+
 
 asyncio.run(main())
